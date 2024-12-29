605581aa7727805dee8f5c6a69f2f0413aa40dcc
Thanospap1968 <thanasis.papanastasiou@gmail.com>
2024-12-29 13:03:18 +0200

    Add files via upload
    initial

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import main
+
+
+    
+df = main.read_map("map.xlsx")
+typoi = df["ΤΥΠΟΣ"].sort_values().unique()
+
+st.title("myDATA Συνδυασμοί Παραστατικών")
+st.write("Prolink Data ΕΠΕ")
+
+st.selectbox(
+    "Επιλογή τύπου Ε3",
+    typoi,
+    index=None,
+    placeholder="Επιλέξτε τύπο Ε3",
+    key="typoi_box"
+
+)
+
+if st.session_state.typoi_box != None:
+    st.dataframe(
+        data=df[df['ΤΥΠΟΣ'] == st.session_state.typoi_box],
+        hide_index=True,
+        column_order=("ΠΑΡΑΣΤΑΤΙΚΟ", "ΚΑΤΗΓΟΡΙΑ"),
+        column_config={
+            "ΠΑΡΑΣΤΑΤΙΚΟ": st.column_config.Column(
+                width="medium"
+            ),
+            "ΚΑΤΗΓΟΡΙΑ": st.column_config.Column(
+                width="large"
+            ),
+        }
+    )
+
+
+
---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def read_map(excel_file):
+    df_map = pd.read_excel(excel_file)
+    return df_map
+
